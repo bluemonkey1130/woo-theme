83097e59631cf38346fc1660dec879606cc011b5
George Hawthorne <bluemonkey1130@gmail.com>
2023-02-16 11:53:55 +1000

    working v 1
    Signed-off-by: George Hawthorne <bluemonkey1130@gmail.com>

--- home.php
+++ home.php
@@ -6,7 +6,18 @@ function removeWhitespace($buffer)
 
 ob_start('removeWhitespace');
 get_header();
-
+//phpinfo();
+//$order_id = 865; // Replace with the ID of the order you want to retrieve the item ID for
+//$order = wc_get_order( $order_id );
+//$items = $order->get_items();
+//
+//if ( ! empty( $items ) ) {
+//    $first_item = reset( $items );
+//    $order_item_id = $first_item->get_id();
+//    echo "Order Item ID: " . $order_item_id;
+//} else {
+//    echo "No items found in order";
+//}
 
 $posts = new WP_Query(array(
     'post_type' => 'post',
